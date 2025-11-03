ae6b501772edf5c96e1179c51977804fd41a2c60
Coder-Lucas <lucas-2012@qq.com>
2025-11-03 15:37:50 +0800

    修复manifest.json路径配置问题 补齐PWA图标 新增SW注册

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,27 +1,38 @@
-"use strict";
+(() => {
+    "use strict";
 
-function addItem() {
-    alert("add按钮编写中");
-    return void 0;
-}
-
-function removeItem() {
-    alert("remove按钮编写中");
-    return void 0;
-}
-
-(function main() {
+    if ("serviceWorker" in navigator) {
+        addEventListener("load", async () => {
+            try {
+                const registration = await navigator.serviceWorker.register("../sw.js", {
+                    scope: "../",
+                    type: "module"
+                });
+                console.info("SW注册成功 PWA离线功能已可用")
+            } catch (error) {
+                console.error("SW注册失败 PWA离线功能不可用 ", error);
+            }
+        });
+    }
     switch (document.body.className) {
         case "index":
             let addBtn = document.querySelector(".add");
             let removeBtn = document.querySelector(".remove");
-            addBtn.onclick = addItem;
-            removeBtn.onclick = removeItem;
+            addBtn.onclick = () => {
+                alert("add按钮编写中");
+                return void 0;
+            };
+            removeBtn.onclick = () => {
+                alert("remove按钮编写中");
+                return void 0;
+            };
             break;
         case "setting":
             break;
         case "achvs":
             break;
+        case "about":
+            break;
     }
     return void 0;
-}());
+})();
