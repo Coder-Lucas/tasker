bbec6736af849c760b31eabac1f47933facedce6
Coder-Lucas <lucas-2012@qq.com>
2025-11-03 21:13:32 +0800

    添加更多日志消息

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,7 @@
 (() => {
     "use strict";
 
+    console.info("开始注册SW")
     if ("serviceWorker" in navigator) {
         addEventListener("load", async () => {
             try {
@@ -13,25 +14,30 @@
                 console.error("SW注册失败 PWA离线功能不可用 ", error);
             }
         });
+    } else {
+        console.warn("该浏览器或设备暂不支持SW")
     }
+    console.info("检测当前所在网页")
     switch (document.body.className) {
         case "index":
-            let addBtn = document.querySelector(".add");
-            let removeBtn = document.querySelector(".remove");
-            addBtn.onclick = () => {
+            console.info("执行index.html代码")
+            document.querySelector(".add").onclick = () => {
                 alert("add按钮编写中");
                 return void 0;
             };
-            removeBtn.onclick = () => {
-                alert("remove按钮编写中");
-                return void 0;
+            document.querySelector(".remove").onclick = () => {
+            alert("remove按钮编写中");
+            return void 0;
             };
             break;
-        case "setting":
+        case "settings":
+            console.info("执行settings.html代码")
             break;
         case "achvs":
+            console.info("执行achvs.html代码")
             break;
         case "about":
+            console.info("执行about.html代码")
             break;
     }
     return void 0;
