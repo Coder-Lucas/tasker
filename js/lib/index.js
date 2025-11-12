afebc7f5d24003e6624356eee1a60284b5db80ff
Coder-Lucas <lucas-2012@qq.com>
2025-11-12 17:01:08 +0800

    首页新增清空按钮 优化网页结构

--- js/lib/index.js
+++ js/lib/index.js
@@ -3,16 +3,24 @@
 export default () =>
 {
     console.info("执行index.html代码");
+    const tb = document.querySelector(".table-body");
     document.querySelector(".add").addEventListener("click", () =>
     {
         console.info("add按钮被点击");
+        alert("编写中 清空按钮已可用");
     });
     document.querySelector(".remove").addEventListener("click", () =>
     {
         console.info("remove按钮被点击");
+        alert("编写中 清空按钮已可用");
     });
     document.querySelector(".clear").addEventListener("click", () =>
     {
         console.info("clear按钮被点击");
+        const empty = document.createElement("div");
+        empty.className = "empty";
+        empty.textContent = "暂无任务";
+        tb.replaceChildren();
+        tb.appendChild(empty);
     });
 }
