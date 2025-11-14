e27e3d314ec370eae7536f655220c1209caaf6ef
Coder-Lucas <lucas-2012@qq.com>
2025-11-14 17:56:05 +0800

    新增重定向规则 关于页面新增前瞻内容部分 当前为API测试

--- js/lib/about.js
+++ js/lib/about.js
@@ -3,4 +3,16 @@
 export default () =>
 {
     console.info("执行about.html代码");
+    document.querySelector(".api").addEventListener("click", () =>
+    {
+        fetch("/api/index").then((res) =>
+        {
+            res.text().then(data =>
+            {
+                document.querySelector(".test").textContent = data;
+            });
+        });
+    }, {
+        once: true,
+    });
 }
